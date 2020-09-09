7e872bf83d1ec8992a425b8a6c931e3408809380
Andrew Suderman <andrew@sudermanjr.com>
2020-09-09 13:32:46 -0600

    client certs

--- README.md
+++ README.md
@@ -4,20 +4,22 @@ This repository contains the code necessary to run my Kubevirtual demo of Agones
 
 ## Steps
 
-1. Build GKE Clusters (done pre-talk)
-1. Installing Agones with Helm on the GKE clusters (1 min)
-1. Setup allocation services (2 min)
-1. Connect clusters to GCGS Realm using gcloud commands (1 min)
-1. Deploy the Fleet of Gameservers (3 min)
-1. Allocate a Gameserver for use by client (5 min)
-1. Connect to the gameserver, start game, end game (5 min)
+1. Build GKE Clusters
+1. Installing Agones with Helm on the GKE clusters
+1. Setup allocation services
+1. Connect clusters to GCGS Realm using gcloud commands
+1. Deploy the Fleet of Gameservers
+1. Allocate a Gameserver for use by client
+1. Connect to the gameserver, start game, end game
 
 ## Using this Repository
 
 Required Tools:
 
 * [Terraform](https://www.terraform.io/) - at least version `0.12.29`
-* [Reckoner](https://github.com/fairwindsops/reckoner) - at least version `3.2.1
+* [Reckoner](https://github.com/fairwindsops/reckoner) - at least version `4.2.0`
+* [gcloud cli](https://cloud.google.com/sdk/gcloud/)
+* [jq](https://stedolan.github.io/jq/)
+* `openssl` for generating client certificates
 
-_NOTE: All of these tools can be installed via [asdf](https://asdf-vm.com/) and I have included a .tool-versions file in this repo_
-
+_NOTE: Some of these tools can be installed via [asdf](https://asdf-vm.com/) and I have included a .tool-versions file in this repo_
--- demo.sh
+++ demo.sh
@@ -5,11 +5,11 @@
 ########################
 . .demo-magic.sh
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Terraform
 p '# Run the Terraform'
-pei 'cd terraform && terraform init && terraform apply -auto-approve'
+pei 'cd terraform; terraform init && terraform apply -auto-approve; cd ..'
 
 # Istio
 p '# Enable Istio on both clusters'
@@ -18,7 +18,7 @@ pei 'gcloud beta container clusters update agones-usc1-blue --update-addons=Isti
 
 # Kubeconfigs
 p '# Get access to the clusters and rename the contexts to more usable'
-export KUBECONFIG="${DIR}/kubeconfig"
+export KUBECONFIG="${REPO_DIR}/kubeconfig"
 pei 'gcloud container clusters get-credentials agones-usc1-green --region us-central1'
 pei 'kubectl config rename-context gke_agones-demo-280722_us-central1_agones-usc1-green green'
 
@@ -29,7 +29,26 @@ pei 'kubectl config get-contexts'
 
 # Install Agones and Cert Manager using Reckoner in each cluster
 p '# Install Agones and Cert-Manager using Reckoner in each cluster'
-COURSE_FILE='${DIR}/resources/course.yaml'
+COURSE_FILE='${REPO_DIR}/resources/course.yaml'
 pei "kubectl config use-context blue; reckoner plot ${COURSE_FILE} -a"
 pei "kubectl config use-context green; reckoner plot ${COURSE_FILE} -a"
 
+pei "kubectl --context blue get po,svc -n agones-system"
+pei "kubectl --context green get po,svc -n agones-system"
+
+# Register the clusters
+p '# Register the clusters with the realm'
+pei 'gcloud beta game servers clusters create usc1-blue --realm=united-states --gke-cluster="projects/agones-demo-280722/locations/us-central1/clusters/agones-usc1-blue" --namespace=gameserver --no-dry-run'
+pei 'gcloud beta game servers clusters create usc1-green --realm=united-states --gke-cluster="projects/agones-demo-280722/locations/us-central1/clusters/agones-usc1-green" --namespace=gameserver --no-dry-run'
+
+# Show existing gameservers
+p '# Since we already deployed a fleet to the realm, we should start to see gameservers'
+pei 'kubectl --context green get fleet,gameserver -n gameserver'
+pei 'kubectl --context blue get fleet,gameserver -n gameserver'
+
+# Generate a client certificate and add it to the allowed list
+p '# If needed, create a client cert. Then add that cert to the allow list in the Agones allocator'
+pei 'if [ ! -f client.crt ]; then openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout client.key -out client.crt; fi'
+PATCH="{\"data\": {\"client.crt\": \"$(cat client.crt | base64)\"}}"
+pei 'kubectl --context green patch secret allocator-tls-ca --type merge -p "$PATCH"'
+pei 'kubectl --context blue patch secret allocator-tls-ca --type merge -p "$PATCH"'
